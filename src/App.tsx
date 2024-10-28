4b716faba821e1cdb0418a802ca868fb68c237c4
Wkatir <110741874+wkatir@users.noreply.github.com.>
2024-10-28 07:48:37 -0600

    feat: adding new directories to the project and files

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className="bg-white dark:bg-slate-800 rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl">App</div>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-   content: ["./src/**/*.{html,js,tsx,ts}"],
+  content: ["./src/**/*.{html,tsx,ts}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
